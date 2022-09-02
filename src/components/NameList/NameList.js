22412e6c2485104fd0f6b4674413d6609fdf18e9
charith1997 <charithmadu97@gmail.com>
2022-09-02 23:35:26 +0530

    list completed

--- src/components/NameList/NameList.js
+++ src/components/NameList/NameList.js
@@ -1,32 +1,30 @@
 import React, { useEffect, useState } from "react";
 import NameItem from "./NameItem";
 
-const list = [
-  {
-    id: { value: Math.random() },
-    name: { title: "Mr", first: "Gerardo", last: "Suarez" },
-    location: { country: "Spain" },
-    email: "gerardo.suarez@example.com",
-    dob: { date: "1994-07-08T14:34:36.061Z", age: 28 },
-    picture: {
-      large: "https://randomuser.me/api/portraits/men/86.jpg",
-    },
-  },
-  {
-    id: { value: Math.random() },
-    name: { title: "Miss", first: "Leni", last: "Rue" },
-    location: { country: "Norway" },
-    email: "leni.rue@example.com",
-    dob: { date: "1998-07-09T08:57:26.803Z", age: 28 },
-    picture: {
-      large: "https://randomuser.me/api/portraits/women/87.jpg",
-    },
-  },
-];
-
 const NameList = () => {
-  const [nameList, setNameList] = useState(list);
   const [loadData, setLoadData] = useState(false);
+  const [nameList, setNameList] = useState([
+    {
+      id: { value: Math.random() },
+      name: { title: "Mr", first: "Gerardo", last: "Suarez" },
+      location: { country: "Spain" },
+      email: "gerardo.suarez@example.com",
+      dob: { date: "1994-07-08T14:34:36.061Z", age: 28 },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/86.jpg",
+      },
+    },
+    {
+      id: { value: Math.random() },
+      name: { title: "Miss", first: "Leni", last: "Rue" },
+      location: { country: "Norway" },
+      email: "leni.rue@example.com",
+      dob: { date: "1998-07-09T08:57:26.803Z", age: 28 },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/87.jpg",
+      },
+    },
+  ]);
 
   useEffect(() => {
     fetch("https://randomuser.me/api")

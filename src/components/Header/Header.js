5c825511dcb734405661ed7d7e2006f463993d3e
charith1997 <charithmadu97@gmail.com>
2022-09-02 21:39:40 +0530

    Header Component Implemented

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header = () => {
-  return <></>;
+  return (
+    <>
+      <nav class="navbar navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1">Name List</span>
+      </nav>
+    </>
+  );
 };
 
 export default Header;

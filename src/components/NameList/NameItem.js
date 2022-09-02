efc973b0cb7cd7cdc65d75adfe981358dd165153
charith1997 <charithmadu97@gmail.com>
2022-09-02 23:28:43 +0530

    NameList update implemented

--- src/components/NameList/NameItem.js
+++ src/components/NameList/NameItem.js
@@ -5,18 +5,19 @@ const NameItem = (props) => {
   return (
     <>
       {props.list.map((item) => (
-        <div className="list-group-item" key={item.id}>
+        <div className="list-group-item" key={item.id.value}>
           <div className="row">
             <div className="col-lg-2">
               <img
                 className="boarder rounded-circle shadow-sm"
-                src={item.img}
+                src={item.picture.large}
               ></img>
             </div>
             <div className="col-lg-10 align-self-center">
-              <p>{`${item.name.first_name} ${item.name.last_name}`}</p>
+              <p>{`${item.name.first} ${item.name.last}`}</p>
               <p>
-                {item.country} | {moment(item.dob).format("DD-MM-YYYY")}
+                {item.location.country} |{" "}
+                {moment(item.dob.date).format("DD-MM-YYYY")}
               </p>
               <p>{item.email}</p>
             </div>
--- src/components/NameList/NameList.js
+++ src/components/NameList/NameList.js
@@ -1,34 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NameItem from "./NameItem";
 
+const list = [
+  {
+    id: { value: Math.random() },
+    name: { title: "Mr", first: "Gerardo", last: "Suarez" },
+    location: { country: "Spain" },
+    email: "gerardo.suarez@example.com",
+    dob: { date: "1994-07-08T14:34:36.061Z", age: 28 },
+    picture: {
+      large: "https://randomuser.me/api/portraits/men/86.jpg",
+    },
+  },
+  {
+    id: { value: Math.random() },
+    name: { title: "Miss", first: "Leni", last: "Rue" },
+    location: { country: "Norway" },
+    email: "leni.rue@example.com",
+    dob: { date: "1998-07-09T08:57:26.803Z", age: 28 },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/87.jpg",
+    },
+  },
+];
+
 const NameList = () => {
-  const nameList = [
-    {
-      id: Math.random(),
-      name: {
-        first_name: "Gerardo",
-        last_name: "Suarez",
-      },
-      country: "Spain",
-      email: "gerardo.suarez@example.com",
-      dob: "1994-07-08T14:34:36.061Z",
-      img: "https://randomuser.me/api/portraits/men/86.jpg",
-    },
-    {
-      id: Math.random(),
-      name: {
-        first_name: "Leni",
-        last_name: "Rue",
-      },
-      country: "Norway",
-      email: "leni.rue@example.com",
-      dob: "1998-07-09T08:57:26.803Z",
-      img: "https://randomuser.me/api/portraits/women/87.jpg",
-    },
-  ];
+  const [nameList, setNameList] = useState(list);
+  const [loadData, setLoadData] = useState(false);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api")
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setNameList(() => [...nameList, responseData.results[0]]);
+      });
+  }, [loadData]);
+
+  const addNameHandler = () => {
+    setLoadData(!loadData);
+
+    // setNameList(() => nameList.concat(newName));
+    // setNameList((nameList) => [...nameList, newName]);
+  };
 
   return (
     <div className="container mt-4">
+      <button className="btn btn-primary mb-4" onClick={addNameHandler}>
+        Add Name
+      </button>
       <NameItem list={nameList} />
     </div>
   );
